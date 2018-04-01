da1a2eb1205550eaa997725ba91ee02cc156e074
Ahmid <ahd.hjz@gmail.com>
2018-04-01 20:16:51 +0300

    preparing client

---
+++ Client/index.js
@@ -0,0 +1,8 @@
+var StreamTweets = require('stream-tweets');
+var credentials = require('./api-keys').twitterKeys;
+ 
+var st = new StreamTweets(credentials);
+
+st.stream('Javascript', function(results){
+    console.log(results); 
+});
