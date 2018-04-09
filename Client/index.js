c764d99e3a865626c3dd78e070bf5f1108e12b0d
Ahmid <ahd.hjz@gmail.com>
2018-04-09 14:56:06 +0300

    Developed NERService

--- Client/index.js
+++ Client/index.js
@@ -1,30 +1,25 @@
+const fetch = require("node-fetch");
 var StreamTweets = require('stream-tweets');
 var request = require('request');
 var credentials = require('./api-keys').twitterKeys;
 
+
 var st = new StreamTweets(credentials);
 
-
-st.stream('Trump', function(results){
-    //console.log(results.body); 
-
-    var text =  {
-      'text': results.body,
-      'model': 'en'
-    };    
-
-    var options = {
-      url: 'http://127.0.0.1:8000/ent',
-      headers : {"content-type": "application/json"},
+function getEntities(text, model) {
+  const options = {
       method: 'POST',
-      json: true,
-      body : text
-    };
-    
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body) // Print the shortened url.
-      }
-    });
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify({ text, model })
+  };
+  fetch('http://127.0.0.1:8000/ent', options)
+      .then(res => res.json())
+      .then(entities => {
+          console.log(entities);
+      });
+}
 
-});
+ st.stream('Trump', function(results){
+  getEntities(results.body, 'en_core_web_sm');
+ });
--- NERService/app.py
+++ NERService/app.py
@@ -1,10 +1,67 @@
-from displacy_service.server import APP, get_model
+from __future__ import unicode_literals
 
-# Pre-load English model only, to save memory
-get_model('en_core_web_sm')
+import hug
+from hug_middleware_cors import CORSMiddleware
+import spacy
+from elasticsearch import Elasticsearch
+import json
+import requests
+
+
+MODELS = {
+    'en_core_web_sm': spacy.load('en_core_web_sm'),
+    # 'de_core_news_sm': spacy.load('de_core_news_sm'),
+    # 'es_core_news_sm': spacy.load('es_core_news_sm'),
+    # 'pt_core_news_sm': spacy.load('pt_core_news_sm'),
+    # 'fr_core_news_sm': spacy.load('fr_core_news_sm'),
+    # 'it_core_news_sm': spacy.load('it_core_news_sm'),
+    # 'nl_core_news_sm': spacy.load('nl_core_news_sm')
+}
+
+
+def get_model_desc(nlp, model_name):
+    """Get human-readable model name, language name and version."""
+    lang_cls = spacy.util.get_lang_class(nlp.lang)
+    lang_name = lang_cls.__name__
+    model_version = nlp.meta['version']
+    return '{} - {} (v{})'.format(lang_name, model_name, model_version)
+
+
+@hug.get('/models')
+def models():
+    return {name: get_model_desc(nlp, name) for name, nlp in MODELS.items()}
+
+
+@hug.post('/dep')
+def dep(text: str, model: str, collapse_punctuation: bool=False,
+        collapse_phrases: bool=False):
+    """Get dependencies for displaCy visualizer."""
+    nlp = MODELS[model]
+    doc = nlp(text)
+    if collapse_phrases:
+        for np in list(doc.noun_chunks):
+            np.merge(tag=np.root.tag_, lemma=np.root.lemma_,
+                     ent_type=np.root.ent_type_)
+    options = {'collapse_punct': collapse_punctuation}
+    return spacy.displacy.parse_deps(doc, options)
+
+
+@hug.post('/ent')
+def ent(text: str, model: str):
+    """Get entities for displaCy ENT visualizer."""
+    nlp = MODELS[model]
+    doc = nlp(text)
+
+    for ent in doc.ents:
+        es.index(index='test_twitter', doc_type='words', body={'tag': ent.text})
+    # return [{'text': ent.text, 'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_}
+    #         for ent in doc.ents]
+
 
 if __name__ == '__main__':
-    from wsgiref import simple_server
-    httpd = simple_server.make_server('127.0.0.1', 8000, APP)
-    httpd.serve_forever()
-
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    r = requests.get('http://localhost:9200')
+    import waitress
+    app = hug.API(__name__)
+    app.http.add_middleware(CORSMiddleware(app))
+    waitress.serve(__hug_wsgi__, port=8000)
